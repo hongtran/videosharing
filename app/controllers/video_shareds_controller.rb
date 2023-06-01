496de7196aa720409a197c370c38cad31db47ccd
hong tran <hong@elsanow.io>
2023-06-02 00:37:10 +0700

    Fix broadcast

--- app/controllers/video_shareds_controller.rb
+++ app/controllers/video_shareds_controller.rb
@@ -9,7 +9,7 @@ class VideoSharedsController < ApplicationController
         video_shared = @current_user.video_shareds.new(video_shared_params)
         if video_shared.save
             # broadcast to all other user logged this shared video
-            ActionCable.server.broadcast 'notifications_channel', title: video_shared.title, email: @current_user.email
+            ActionCable.server.broadcast 'notifications_channel', {title: video_shared.title, email: @current_user.email}
             render json: video_shared, status: :created
         else
             render json: { error: video_shared.errors.full_messages }, status: :unprocessable_entity
