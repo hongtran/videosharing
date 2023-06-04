0a2c01f2e1b8a7b6fa505ecc58ec3465b71d13b4
hong tran <hong@elsanow.io>
2023-06-04 20:17:39 +0700

    remove

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,10 +4,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "should get signup" do
   #   get signup_url 
   #   assert_response :success
-  # end
-
-  test "should get login" do
-    get login_url, params: { email: 'hong@example.com', password: '123456' }
-    assert_response :success
-  end
 end
