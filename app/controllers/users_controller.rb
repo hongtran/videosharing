acedc8b4592450ab7b63ccc78d9a01c5ab63c54c
hong tran <hong@elsanow.io>
2023-06-05 20:29:56 +0700

    allow sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
     def signup
       user = User.new(user_params)
       if user.save
-        render json: { message: 'User created successfully' }, status: :created
+        token = user.generate_token
+        user_data = { id: user.id, email: user.email, name: user.name }
+        render json: { token: token, user: user_data }, status: :created
       else
         render json: { error: user.errors.full_messages }, status: :unprocessable_entity
       end
@@ -11,12 +13,23 @@ class UsersController < ApplicationController
   
     def login
       user = User.find_by(email: params[:email])
-      if user&.authenticate(params[:password])
-        token = user.generate_token
-        user_data = { id: user.id, email: user.email, name: user.name }
-        render json: { token: token, user: user_data }, status: :ok
+      if user
+        if user.authenticate(params[:password])
+          token = user.generate_token
+          user_data = { id: user.id, email: user.email, name: user.name }
+          render json: { token: token, user: user_data }, status: :ok
+        else
+          render json: { error: 'Invalid email or password' }, status: :unauthorized
+        end
       else
-        render json: { error: 'Invalid email or password' }, status: :unauthorized
+        user = User.new(user_params)
+        if user.save
+          token = user.generate_token
+          user_data = { id: user.id, email: user.email, name: user.name }
+          render json: { token: token, user: user_data }, status: :created
+        else
+          render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+        end
       end
     end
   
